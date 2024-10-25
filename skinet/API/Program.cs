9c85009349d6e712c3dac5270449597ce595293e
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2024-10-25 16:38:19 -0500

    Defined Product Repository.

--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Intefaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
